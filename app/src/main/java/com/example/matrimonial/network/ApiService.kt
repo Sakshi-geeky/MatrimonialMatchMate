543b7dd264c2699b1bff64474c508121c8bd5316
sakshi <sakshichavan041@gmail.com>
2025-06-15 21:05:10 +0530

    Api Interface created

--- app/src/main/java/com/example/matrimonial/network/ApiService.kt
+++ app/src/main/java/com/example/matrimonial/network/ApiService.kt
@@ -5,6 +5,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface ApiService {
+
     @GET("api/?results=100")
     suspend fun getUsers(): Response<UserResponse>
 }
--- app/src/main/java/com/example/matrimonial/network/RetrofitInstance.kt
+++ app/src/main/java/com/example/matrimonial/network/RetrofitInstance.kt
@@ -4,6 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
+
     val api: ApiService by lazy {
         Retrofit.Builder()
             .baseUrl("https://randomuser.me/")
